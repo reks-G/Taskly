da4d1c48d92bdb8067df6b9afeef5d46ad006cfb
reks-G <kabanqwefordora@gmail.com>
2026-05-31 05:56:04 +0700

    upd

--- main.py
+++ main.py
@@ -4,18 +4,13 @@ from bot import bot, start_scheduler
 from database import init_db
 
 def run_bot():
-    print('Bot started and running...')
     bot.infinity_polling()
 
 if __name__ == '__main__':
-    print('Initializing database...')
     init_db()
-    print('Starting scheduler...')
     start_scheduler()
     
-    print('Starting bot thread...')
     bot_thread = threading.Thread(target=run_bot, daemon=True)
     bot_thread.start()
     
-    print('Starting Flask app...')
     app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
